9f81cebe77d4bf9dcac9fa2e5c274e141b676e30
Lynsey Kovar <lk82153@xfer3.gacrc.uga.edu>
2019-06-25 21:50:44 -0400

    add 1a_PairwiseAlignmentComponent.sh which is the master script for pairwise alignment of whole genomes

---
+++ scripts/1a_PairwiseAlignmentComponent.sh
@@ -0,0 +1,95 @@
+#! /bin/bash
+
+# Script to do pairwise multiple alignment of two genomes
+
+
+# Software downloaded from UCSC, from http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/
+UCSC=$HOME/Software/Aligners/UCSC_utils
+
+# arguments
+tname=$1
+target_genome=$2
+target_size=$3
+qname=$4
+query_genome=$5
+query_size=$6
+aligndir=$7
+
+targetdir=$aligndir/1a_${tname}_split
+querydir=$aligndir/1a_${qname}_split
+pairdir=$aligndir/1b_${tname}_${qname}_pairwise
+chaindir=$aligndir/1e_${tname}_${qname}_chains
+
+if [ ! -e $targetdir ]; then mkdir $targetdir; fi
+if [ ! -e $querydir ]; then mkdir $querydir; fi
+if [ ! -e $pairdir ]; then mkdir $pairdir; fi
+if [ ! -e $chaindir ]; then mkdir $chaindir; fi
+
+target_sizes=$chaindir/1e_$tname.combined.sizes
+query_sizes=$chaindir/1e_$qname.combined.sizes
+
+# # # Split target genome by segments
+# outprefix=$targetdir/${tname}
+# python3 1a_CutGenomesBySize.py -i $target_genome -o $outprefix --rename $tname --min 1000 --winsize $target_size #--debug
+# # 
+# # # # Split query genome by segments
+# outprefix=$querydir/${qname}
+# python3 1a_CutGenomesBySize.py -i $query_genome -o $outprefix --rename $qname --min 1000 --winsize $query_size #--debug
+# 
+# 
+# # Do all pairwise target-query alignments
+
+# lastz_commands=$aligndir/1b_${tname}_${qname}.lastz_commands.txt
+# chain_commands=$aligndir/1c_${tname}_${qname}.chain_commands.txt
+# echo "echo -e #######\nRunning Pairwise alignments\n#######" > $lastz_commands
+# echo "echo -e #######\nConverting lastz output to chains\n#######" > $chain_commands
+# i=0
+# for target_win in $targetdir/${tname}*.fa; do
+#     for query_win in $querydir/${qname}*.fa; do
+#     
+#       axtfile=$pairdir/1c_${tname}_${qname}.$i.axt 
+#       chainfile=$pairdir/1d_${tname}_${qname}.$i.chain
+#       
+#       # Align with LASTZ and convert to a chain file
+#       echo "lastz $target_win $query_win --ambiguous=iupac --format=axt --inner=2000 --xdrop=9400 --gappedthresh=3000 --hspthresh=2200 > $axtfile" >> $lastz_commands
+#       echo "$UCSC/axtChain -linearGap=loose $axtfile -faT $target_win -faQ $query_win $chainfile" >> $chain_commands
+#       
+#       i=$(($i+1))
+#       
+#     done
+# done
+# cat $lastz_commands | parallel --progress
+# cat $chain_commands | parallel --progress
+# 
+# # # ## Get sizes of chromosomes/chromosome segments ##
+# cat $targetdir/*.fa > $chaindir/1e_$tname.combined.fa
+# faSize $chaindir/1e_$tname.combined.fa -detailed > $target_sizes
+# rm $chaindir/1e_$tname.combined.fa
+# 
+# cat $querydir/*.fa > $chaindir/1e_$qname.combined.fa
+# faSize $chaindir/1e_$qname.combined.fa -detailed > $query_sizes
+# rm $chaindir/1e_$qname.combined.fa
+# 
+# 
+# 
+# # Sort and filter chains ##    
+# find $pairdir -maxdepth 1 -name "*.chain" > $chaindir/1e_${tname}_${qname}_all.chains.txt
+# $UCSC/chainMergeSort -inputList=$chaindir/1e_${tname}_${qname}_all.chains.txt > $chaindir/1e_${tname}_${qname}_all.chain.merge
+# $UCSC/chainPreNet $chaindir/1e_${tname}_${qname}_all.chain.merge $target_sizes $query_sizes $chaindir/1e_${tname}_${qname}_all.chain.merge.pre
+# 
+# # ## Net the chains ##
+# $UCSC/chainNet $chaindir/1e_${tname}_${qname}_all.chain.merge.pre -minSpace=1 $target_sizes $query_sizes $chaindir/1f_${tname}_${qname}.target_$tname.net $chaindir/1f_${tname}_${qname}.query_$qname.net
+# 
+# # ## Add synteny info to nets ##
+# $UCSC/netSyntenic $chaindir/1f_${tname}_${qname}.target_$tname.net $chaindir/1f_${tname}_${qname}.target_$tname.noClass.net
+# 
+# 
+# # Make 2bit files
+# $UCSC/faToTwoBit $targetdir/${tname}*.fa $chaindir/1g_$tname.seqs.2bit
+# $UCSC/faToTwoBit $querydir/${qname}*.fa $chaindir/1g_$qname.seqs.2bit
+
+# # Convert to MAF files
+# $UCSC/netToAxt $chaindir/1f_${tname}_${qname}.target_$tname.noClass.net $chaindir/1e_${tname}_${qname}_all.chain.merge.pre $chaindir/1g_$tname.seqs.2bit $chaindir/1g_$qname.seqs.2bit \
+#   $chaindir/1h_${tname}_${qname}.multialign.axt
+$UCSC/axtSort $chaindir/1h_${tname}_${qname}.multialign.axt $chaindir/1h_${tname}_${qname}.multialign.sorted.axt
+$UCSC/axtToMaf $chaindir/1h_${tname}_${qname}.multialign.sorted.axt $target_sizes $query_sizes $chaindir/1h_${tname}_${qname}.multialign.sorted.maf
