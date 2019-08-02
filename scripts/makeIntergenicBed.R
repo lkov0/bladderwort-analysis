575f3189edf5fbb6d337c127774cbad20eeec010
Lynsey Kovar <lk82153@sapelo2-sub2.ecompute>
2019-08-02 15:31:02 -0400

    formatting improvement

--- scripts/makeIntergenicBed.R
+++ scripts/makeIntergenicBed.R
@@ -15,16 +15,23 @@ cat('Making bedfiles for intergenic regions of convergent, divergent, and parall
 
 genePairs <- read.table(args$inputFile, header=T, sep="\t")
 
-genePairs.parallel <- subset(genePairs, orientation == "parallel")
-genePairs.convergent <- subset(genePairs, orientation == "convergent")
-genePairs.divergent <- subset(genePairs, orientation == "divergent")
-    
-genePairs.parallelIntergenic <- data.frame(chr = genePairs.parallel$chr1, start = genePairs.parallel$stop1, stop = genePairs.parallel$start2, name=paste(genePairs.parallel$geneId1, genePairs.parallel$geneId2, sep="."))
-genePairs.convergentIntergenic <- data.frame(chr = genePairs.convergent$chr1, start = genePairs.convergent$stop1, stop = genePairs.convergent$start2, name=paste(genePairs.convergent$geneId1, genePairs.convergent$geneId2, sep="."))
-genePairs.divergentIntergenic <- data.frame(chr = genePairs.divergent$chr1, start = genePairs.divergent$stop1, stop = genePairs.divergent$start2, name=paste(genePairs.divergent$geneId1, genePairs.divergent$geneId2, sep="."))    
+genePairs.bed <- data.frame(chr = genePairs$chr1, start = genePairs$stop1, stop = genePairs$start2, name=paste(genePairs$geneId1, genePairs$geneId2, sep="."))
 
-write.table(genePairs.parallelIntergenic, paste(args$outputStem, ".intergenic.parallel.bed", sep=""), row.names=F, col.names=F, sep="\t", quote=F)
-write.table(genePairs.convergentIntergenic, paste(args$outputStem, ".intergenic.convergent.bed", sep=""), row.names=F, col.names=F, sep="\t", quote=F)
-write.table(genePairs.divergentIntergenic, paste(args$outputStem, ".intergenic.divergent.bed", sep=""), row.names=F, col.names=F, sep="\t", quote=F)
+write.table(genePairs.bed, paste(args$outputStem, ".bed", sep=""), row.names=F, col.names=F, sep="\t", quote=F)
+
+### make it so everything below here happens if an additional option (splitOrientation == TRUE)
+
+# genePairs <- read.table(args$inputFile, header=T, sep="\t")
+# 
+# genePairs.parallel <- subset(genePairs, orientation == "parallel")
+# genePairs.convergent <- subset(genePairs, orientation == "convergent")
+# genePairs.divergent <- subset(genePairs, orientation == "divergent")
+#     
+# genePairs.parallelIntergenic <- data.frame(chr = genePairs.parallel$chr1, start = genePairs.parallel$stop1, stop = genePairs.parallel$start2, name=paste(genePairs.parallel$geneId1, genePairs.parallel$geneId2, sep="."))
+# genePairs.convergentIntergenic <- data.frame(chr = genePairs.convergent$chr1, start = genePairs.convergent$stop1, stop = genePairs.convergent$start2, name=paste(genePairs.convergent$geneId1, genePairs.convergent$geneId2, sep="."))
+# genePairs.divergentIntergenic <- data.frame(chr = genePairs.divergent$chr1, start = genePairs.divergent$stop1, stop = genePairs.divergent$start2, name=paste(genePairs.divergent$geneId1, genePairs.divergent$geneId2, sep="."))
+# write.table(genePairs.parallelIntergenic, paste(args$outputStem, ".intergenic.parallel.bed", sep=""), row.names=F, col.names=F, sep="\t", quote=F)
+# write.table(genePairs.convergentIntergenic, paste(args$outputStem, ".intergenic.convergent.bed", sep=""), row.names=F, col.names=F, sep="\t", quote=F)
+# write.table(genePairs.divergentIntergenic, paste(args$outputStem, ".intergenic.divergent.bed", sep=""), row.names=F, col.names=F, sep="\t", quote=F)
 
 
