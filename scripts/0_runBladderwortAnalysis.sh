d9dc004707e35dda8f3c77c397b1dc5eecb61bc3
Lynsey Kovar <lk82153@sapelo2-sub1.ecompute>
2020-01-10 14:28:46 -0500

    comment/uncomment lines

--- scripts/0_runBladderwortAnalysis.sh
+++ scripts/0_runBladderwortAnalysis.sh
@@ -3,14 +3,15 @@
 # Date: Jan 16 2019
 # Purpose: Parent shell script for bladderwort analysis.
 # Software needed: gmap (v2018-07-04), gsnap (v2018-07-04), samtools (v1.6 using htslib 1.6), cufflinks (v2.2.1), ncbi blast (v2.7.1+), meme suite (5.0.4), mummer (v4.0.0.beta2)
+# for interactive session: qsub -I -q s_interq -l walltime=10:00:00 -l nodes=1:ppn=16:inter -l mem=16gb
 ###########################
 
 # parentDir=~/Xfer/Bladderwort
 # scriptDir=~/Xfer/Repositories/bladderwort-analysis/scripts
-parentDir=/scratch/lk82153/jwlab/Bladderwort
-scriptDir=/scratch/lk82153/jwlab/Repositories/bladderwort-analysis/scripts
+parentDir=/scratch/lk82153/jwlab/Bladderwort_3pri
+scriptDir=~/Repositories/bladderwort-analysis/scripts
 dataDir=/work/jawlab/data/bladderwort
-threeprimeDir=$dataDir/nextseq_rna_3prime/
+threeprimeDir=$dataDir/nextseq_rna_3prime
 assemblyDir=$parentDir/1_Assembly
 alignmentDir=$parentDir/2_Alignment
 quantDir=$parentDir/3_Quantification
@@ -18,6 +19,7 @@ analysisDir=$parentDir/4_CandidateMining
 memeDir=$parentDir/5_Meme
 consDir=$parentDir/6_ConservationAnalysis
 
+if [ ! -e $parentDir ]; then mkdir $parentDir; fi
 if [ ! -e $assemblyDir ]; then mkdir $assemblyDir; fi
 if [ ! -e $alignmentDir ]; then mkdir $alignmentDir; fi
 if [ ! -e $quantDir ]; then mkdir $quantDir; fi
@@ -25,8 +27,9 @@ if [ ! -e $analysisDir ]; then mkdir $analysisDir; fi
 if [ ! -e $memeDir ]; then mkdir $memeDir; fi
 if [ ! -e $consDir ]; then mkdir $consDir; fi
 
+refGenome=$dataDir/genomes/utricularia/scaffolds.ugibba_lk.fasta
 
-NPROCS=24
+PROCS=24
 
 # download RNA-Seq reads from NCBI.
 # SRR094438: low coverage, average of 31,500 reads for each condition (~820k total)
@@ -39,9 +42,9 @@ NPROCS=24
 # fastq-dump -O $dataDir --gzip $fastqDir/$file
 # done
 # 
-$parentDir/2_runAlignment.sh $fastqDir $alignmentDir
-# 
-# $scriptDir/3_evaluateGenePairs.sh
+# $scriptDir/2_runAlignment.sh $threeprimeDir $alignmentDir $refGenome $dataDir $PROCS
+
+$scriptDir/3_evaluateGenePairs.sh $parentDir $scriptDir $dataDir $alignmentDir $quantDir
 
 # $scriptDir/4_findMotifs.sh $parentDir $scriptDir $dataDir $analysisDir $memeDir $NPROCS
 
